7b21c105ea4686d3c9f171467ecc21cf47c0cccd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-15 10:54:13 +0100

    Getting the job and the step

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ async function computeStepDuration(stepName) {
 
     // Gets the current workflow
     const runId = github.context.runId
+    const jobName = github.context.job
     console.log(`Run ID: ${runId}`)
-    console.log(`Job: ${github.context.job}`)
+    console.log(`Job: ${jobName.job}`)
 
     const run = await octokit.actions.getWorkflowRun({
         owner: github.context.repo.owner,
@@ -39,7 +40,27 @@ async function computeStepDuration(stepName) {
         run_id: runId
     });
 
-    console.log(`Run: ${run}`)
+    // Looks for the job
+    const job = run.jobs.find((item) => item.name === jobName);
+    if (!job) {
+        throw `Job not found in current workflow: ${jobName}`
+    }
+
+    // Looks for the step
+    const step = job.steps.find((item) => item.name === stepName);
+    if (!step) {
+        throw `Step not found in current job: ${stepName}`
+    }
+
+    // Step information
+    const status = step.status
+    const conclusion = step.conclusion
+    const startedAt = new Date(step.started_at)
+    const completedAt = new Date(step.completed_at)
+    console.log(`Step status: ${status}`)
+    console.log(`Step conclusion: ${conclusion}`)
+    console.log(`Step started at: ${startedAt}`)
+    console.log(`Step completed at: ${completedAt}`)
 
     // TODO Gets the duration
     return 0
